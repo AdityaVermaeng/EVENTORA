05e5c5877886f8f601b1eda9fb40adb112d7622a
AdityaVermaeng <adityafriendrahul@gmail.com>
2026-08-31 23:26:53 +0530

    fix: fix some api endpoints

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 const express = require("express");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
 dotenv.config();
 
-
 const authRoutes = require("./routes/auth");
-const eventRoutes = require(".routes/events");
+const eventRoutes = require("./routes/event");
+const bookingRoutes = require("./routes/booking");
 
-
-
-
-const app= express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 //Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
